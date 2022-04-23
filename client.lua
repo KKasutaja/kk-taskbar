00ef5086308923b5bddefec0692cf1e8640f7229
KKasutaja <76070163+KKasutaja@users.noreply.github.com>
2022-04-23 10:02:45 +0300

    Standalone
    Removal of ox_lib

--- client.lua
+++ client.lua
@@ -1,33 +1,39 @@
 local actionInProgress = false
 
 -- Functions
+local function loadAnimDict(dict)
+	while not HasAnimDictLoaded(dict) do
+		RequestAnimDict(dict)
+		Wait(5)
+	end
+end
+
 local function TaskBar(name, text, time, clip, dict, cb)
+	local playerPed = PlayerPedId()
+
 	if not actionInProgress then
 		actionInProgress = true
 
 		if time > 750 and text then
 			if dict and clip then
-				lib.requestAnimDict(dict); TaskPlayAnim(cache.ped, dict, clip, 8.0, 1.0, -1, 49, 0, 0, 0, 0)
+				loadAnimDict(dict); TaskPlayAnim(playerPed, dict, clip, 8.0, 1.0, -1, 49, 0, 0, 0, 0)
 			elseif not dict and clip then
-				TaskStartScenarioInPlace(cache.ped, clip, 0, true)
+				TaskStartScenarioInPlace(playerPed, clip, 0, true)
 			end
 
 			SendNUIMessage({type = 'ui', display = true, time = time, text = text})
 
 			SetTimeout(time + 100, function()
-				if clip then ClearPedTasks(cache.ped) end
+				if clip then ClearPedTasks(playerPed) end
 				if cb then cb() end
 
 				actionInProgress = false
 			end)
 		else
-			print('[KKF.UI.TaskBar]: Time and text are not correct.')
+			print('[KKF]: Time and text are not correct.')
 		end
 	else
-		lib.notify({
-			description = 'You cannot perform two activities at the same time.',
-			status = 'error'
-		})
+		print('[KKF]: You cannot perform two activities at the same time.')
 	end
 end
 
